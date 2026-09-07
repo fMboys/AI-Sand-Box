e8f2f73895387776267992dad444f2c8fcb7646a
Fahad Mehmood <fahadmehmood009@gmail.com>
2026-09-07 22:33:42 +0500

    Developed first agent for chat conversation

---
+++ Agents/ChatAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+// var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+IChatClient client = new OllamaApiClient(new Uri("http://localhost:11434/"), "llama3.2");
+// Create an agent
+AIAgent assistant = client.AsAIAgent(
+    name: "Assistant",
+    instructions: "You are helpful assistant that remembers our conversation.");
+
+// Create a thread to maintain context
+AgentSession session = await assistant.CreateSessionAsync();
+
+// First message
+var response1 = await assistant.RunAsync("My name is Name.", session);
+Console.WriteLine($"Agent: {response1.Text}");
+
+// Second message
+var response2 = await assistant.RunAsync("What is my name?", session);
+Console.WriteLine($"Agents: {response2.Text}");
+
+// Second message
+var response3 = await assistant.RunAsync("Can you spell it backwards?", session);
+Console.WriteLine($"Agents: {response3.Text}");
---
+++ LocalFoundary/ToolkitChat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ClientModel;
+using System.Text;
+using OpenAI;
+using OpenAI.Chat;
+
+var model = "ministral-3-3b-instruct-2512";
+var baseUrl = "http://localhost:5272/v1/";
+var apiKey = "unused";
+
+OpenAIClientOptions options = new OpenAIClientOptions();
+options.Endpoint = new Uri(baseUrl);
+ApiKeyCredential credential = new ApiKeyCredential(apiKey);
+
+ChatClient client = new OpenAIClient(credential, options).GetChatClient(model);
+
+// here we're building the prompt
+StringBuilder prompt = new StringBuilder();
+prompt.AppendLine("You will analyze the sentiment of the following product reviews. Each line is its own review. Output the sentiment of each review in a bulleted list and then provide a generate sentiment of all reviews. ");
+prompt.AppendLine("I bought this product and it's amazing. I love it!");
+prompt.AppendLine("This product is terrible. I hate it.");
+prompt.AppendLine("I'm not sure about this product. It's okay.");
+prompt.AppendLine("I found this product based on the other reviews. It worked for a bit, and then it didn't.");
+
+// send the prompt to the model and wait for the text completion
+var response = await client.CompleteChatAsync(prompt.ToString());
+Console.WriteLine(response.Value.Content[0].Text);
